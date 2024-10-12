e19afccdc95f6bd84fac924e064e88da05d0f061
BinhAn14 <an100277@donga.edu.vn>
2024-10-12 09:16:07 +0700

    ip

--- B1/router/root.mjs
+++ B1/router/root.mjs
@@ -1,68 +1,16 @@
 import express from "express"
+import HomeController from "../../../controllers/home_controller.mjs"
+import CvController from "../../../controllers/cv_controller.mjs"
 const router = express.Router()
 
-let workerExperience = [{
-        name: "Front End Developer",
-        start_date: "Jan 2014",
-        end_date: "Dec 2015",
-        description: "Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste."
-    },
-    {
-        name: "Web Developer ",
-        start_date: "Mar 2012",
-        end_date: "Dec 2014",
-        description: "Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.",
-    },
-    {
-        name: "Graphic Designer",
-        start_date: "Jun 2010",
-        end_date: "Mar 2012",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-    },
-]
-let education = [{
-        name: "W3Schools.com",
-        start_date: "Forever",
-        end_date: "Forever",
-        description: "Web Development! All I need to know in one place."
-    },
-    {
-        name: "London Business School",
-        start_date: "2013",
-        end_date: "2015",
-        description: "Master Degree",
-    },
-    {
-        name: "School of Coding",
-        start_date: "2010",
-        end_date: "2013",
-        description: "Bachelor Degree.",
-    },
-]
 
-
-
-
-function buildEducation() {
-    let data = '';
-    education.forEach((element) => {
-        data += buildItem(element);
-    });
-    return data;
-}
 
 
 router.get("/about", (req, res) => {
     res.send(`<h1>This is about page<h1>`)
 })
 
-router.get("/", (req, res) => {
-    console.log(req.query)
-    res.render("index", { title: "Home", workerExperience, education })
-});
+router.get("/", HomeController.index);
 
-router.get("/cv/:id", (req, res) => {
-    console.log(req.params)
-    res.render("cv", { title: "CV", workerExperience, education })
-});
+router.get("/cv/:id", CvController.detail);
 export default router;
